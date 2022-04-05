b5083f68f7cd3c8f5c0f31a49533deaa9ada86bf
AdamDang1 <dtdang1@gmail.com>
2022-04-05 18:46:48 -0500

    finished up to part 3 adding functionality

---
+++ server/controller.js
@@ -0,0 +1,17 @@
+const houses = require("./db.json");
+let globalID = 4
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+    deleteHouse: (req, res) => {
+
+    },
+    createHouse: (req, res) => {
+
+    },
+    updateHouse: (req, res) => {
+
+    }
+}
